245134c37d756f104e295df383cac02b479a4a93
moxingwang <xingwang.mo@chinaredstar.com>
2020-03-02 16:45:04 +0800

    init

--- src/main/java/top/moxingwang/mysql/test/MysqlTest1.java
+++ src/main/java/top/moxingwang/mysql/test/MysqlTest1.java
@@ -20,17 +20,22 @@ public class MysqlTest1 {
         dataSource.setQueryTimeout(5);
         dataSource.setLogAbandoned(true);
 
+
         try {
-            // 获得连接:
             conn = dataSource.getConnection();
-            // 编写SQL：
+
+            conn.setAutoCommit(false);
+
             String sql = "select sleep(10) ,shop_id from tx_order where id =1";
             pstmt = conn.prepareStatement(sql);
-            // 执行sql:
+
+
+
             rs = pstmt.executeQuery();
             while (rs.next()) {
                 System.out.println(rs.getInt("id") + "   " + rs.getString("id"));
             }
+            conn.commit();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
